776445304345cc4c7331ddbe48c357ae2a323a8f
autosyslab <iulian@autosyslab.com>
2025-09-04 00:11:01 +0300

    Fix syntax error by removing extra closing div tag

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ export default function App() {
         <div className="bg-gradient-to-b from-[#071420] via-[#0a1625] via-[#0d1a2a] to-[#0d1a2a]">
           <Pricing />
         </div>
-      </main>
       <Footer />
     </div>
   );
